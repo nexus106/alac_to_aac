bcf465310bea4aecfdafcf5e38b0c4b7b6625e80
nexus106 <106nexus@gmail.com>
2024-10-07 14:45:18 +0900

    Refactor: 型ヒントを明記

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import os
 import sys
 import subprocess
-from concurrent.futures import ProcessPoolExecutor
+# from concurrent.futures import ProcessPoolExecutor
 from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 
 
-def duplicate_folder_with_suffix(src_folder, audio_format) -> any:
+def duplicate_folder_with_suffix(src_folder, audio_format) -> str:
     # 複製元のフォルダが存在するか確認
     if not os.path.exists(src_folder):
         raise Exception(f"フォルダ {src_folder} が存在しません")
 
     # フォルダ名を作成（末尾に変換先フォーマットを追加）
-    new_folder = src_folder + "_" + audio_format
+    new_folder: str = src_folder + "_" + audio_format
 
     # 作成するフォルダが既に存在するか確認
     if os.path.exists(new_folder):
@@ -28,10 +28,10 @@ def duplicate_folder_with_suffix(src_folder, audio_format) -> any:
     return new_folder
 
 
-def run_shell_command(xld_binary, file, new_folder, audio_format):
-    command = [xld_binary, "-o", new_folder, file, "-f", audio_format]
+def run_shell_command(xld_binary, file, new_folder, audio_format) -> None:
+    command: list[str] = [xld_binary, "-o", new_folder, file, "-f", audio_format]
     # command = ["afconvert", "-d", audio_format, "-s", encode_mode, "-b", bitrate, file, new_folder]
-    result = subprocess.run(command, capture_output=True, text=True)
+    result: subprocess.CompletedProcess[str] = subprocess.run(command, capture_output=True, text=True)
 
     if result.returncode == 0:
         print(f"変換に成功しました {file}")
@@ -42,9 +42,9 @@ def run_shell_command(xld_binary, file, new_folder, audio_format):
 if __name__ == "__main__":
     # python main.py ./xld test_file aac
     args = sys.argv
-    xld_binary = args[1]  # xldの実行ファイル ./xld
-    src_folder = args[2]  # 元のフォルダパス test_file
-    audio_format = args[3]  # 変換先のフォーマット aac
+    xld_binary: str = args[1]  # xldの実行ファイル ./xld
+    src_folder: str = args[2]  # 元のフォルダパス test_file
+    audio_format: str = args[3]  # 変換先のフォーマット aac
 
     new_folder: str = duplicate_folder_with_suffix(src_folder, audio_format)
     list_file: list[str] = [
